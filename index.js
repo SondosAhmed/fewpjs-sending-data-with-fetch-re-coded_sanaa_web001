909eda5c14884b3400c52205916a016b19df3ca9
Sondos Ahmed <sondosalshami@gmail.com>
2020-08-18 21:02:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+submitData(name , email){
+let formData = {
+  uName: name,
+  uEmail: email
+};
+
+// method: "POST" is missing from the object below
+let configObj = {
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+ const url = "http://localhost:3000/dogs";
+fetch(url, configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object);
+  })
+  .catch(function(error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+  });}
